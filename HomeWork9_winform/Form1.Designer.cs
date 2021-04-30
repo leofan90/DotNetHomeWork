638439eb25ffb4ad5276911a1ca883882c72ba10
Leo Fan <48375324+leofan90@users.noreply.github.com>
2021-04-30 09:42:38 +0800

    Add files via upload

---
+++ HomeWork9_winform/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace HomeWork9_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.initialURLBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rightURLrichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.wrongURLrichTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // initialURLBox1
+            // 
+            this.initialURLBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.initialURLBox1.Location = new System.Drawing.Point(336, 122);
+            this.initialURLBox1.Name = "initialURLBox1";
+            this.initialURLBox1.Size = new System.Drawing.Size(368, 44);
+            this.initialURLBox1.TabIndex = 0;
+            this.initialURLBox1.TextChanged += new System.EventHandler(this.initialURLBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(118, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "初始URL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(779, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始爬虫";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(118, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "已爬取URL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(118, 516);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "错误URL";
+            // 
+            // rightURLrichTextBox1
+            // 
+            this.rightURLrichTextBox1.Location = new System.Drawing.Point(336, 254);
+            this.rightURLrichTextBox1.Name = "rightURLrichTextBox1";
+            this.rightURLrichTextBox1.Size = new System.Drawing.Size(631, 190);
+            this.rightURLrichTextBox1.TabIndex = 5;
+            this.rightURLrichTextBox1.Text = "";
+            this.rightURLrichTextBox1.TextChanged += new System.EventHandler(this.rightURLrichTextBox1_TextChanged);
+            // 
+            // wrongURLrichTextBox2
+            // 
+            this.wrongURLrichTextBox2.Location = new System.Drawing.Point(336, 516);
+            this.wrongURLrichTextBox2.Name = "wrongURLrichTextBox2";
+            this.wrongURLrichTextBox2.Size = new System.Drawing.Size(631, 222);
+            this.wrongURLrichTextBox2.TabIndex = 6;
+            this.wrongURLrichTextBox2.Text = "";
+            this.wrongURLrichTextBox2.TextChanged += new System.EventHandler(this.wrongURLrichTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1286, 779);
+            this.Controls.Add(this.wrongURLrichTextBox2);
+            this.Controls.Add(this.rightURLrichTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.initialURLBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox initialURLTextBox1;
+        private System.Windows.Forms.TextBox initialURLBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rightURLrichTextBox1;
+        private System.Windows.Forms.RichTextBox wrongURLrichTextBox2;
+    }
+}
+
---
+++ HomeWork9_winform/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork9_winform
+{
+    public partial class Form1 : Form
+    {
+        private string startUrl;
+        private string rightUrl;
+        private string wrongUrl;
+        SimpleCrawler myCrawler = new SimpleCrawler();
+        public Form1()
+        {
+            InitializeComponent();
+            initialURLBox1.DataBindings.Add("Text", this, "startUrl");
+            rightURLrichTextBox1.DataBindings.Add("Text", this, "rightUrl");
+            wrongURLrichTextBox2.DataBindings.Add("Text", this, "wrongUrl");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myCrawler.Main(startUrl);
+            for (int i = 0; i < myCrawler.rightUrls.Length; i++)
+            {
+                rightUrl = myCrawler.rightUrls[i];
+                Console.WriteLine(rightUrl);
+            }
+            for (int j = 0; j < myCrawler.wrongUrls.Length; j++)
+            {
+                wrongUrl = myCrawler.wrongUrls[j];
+                Console.WriteLine(wrongUrl);
+            }
+        }
+
+        private void initialURLBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rightURLrichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wrongURLrichTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class SimpleCrawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        internal string[] rightUrls;
+        internal string[] wrongUrls;
+        internal void Main(string startUrl)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                Parse(html);//解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            int w = 0, r = 0;
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0)
+                {
+                    wrongUrls[w] = strRef;
+                    w++;
+                    continue;
+                }
+                if (urls[strRef] == null)
+                {
+                    rightUrls[r] = strRef;
+                    r++;
+                    urls[strRef] = false;
+                }
+            }
+        }
+    }
+}
